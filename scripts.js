381efb8872aa013837f7d44d4a2aa05b20f71937
CarlosAugusto2000 <carlaugust2000@gmail.com>
2026-08-29 18:16:23 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const titulo = document.querySelector('header h1');
-    const musica = new Audio('vinumsabbathi.mp3');
-    titulo.addEventListener
+    const musica = new Audio('/vinumsabbathi.mp3');
+    titulo.addEventListener('click', () => {
         musica.currentTime = 0;
         musica.play().catch(error => {
             console.log('Erro ao tentar tocar o áudio:', error);
