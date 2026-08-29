112f65f7095f1a6e5a3790dccfb95421d72a757d
CarlosAugusto2000 <carlaugust2000@gmail.com>
2026-08-29 18:18:35 -0300

    Update audio file path in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const titulo = document.querySelector('header h1');
-    const musica = new Audio('/vinumsabbathi.mp3');
+    const musica = new Audio('music/vinumsabbathi.mp3');
     titulo.addEventListener('click', () => {
         musica.currentTime = 0;
         musica.play().catch(error => {
