e1349dd206748f6d16b71ec32c9eb00b5ba70cd7
CarlosAugusto2000 <carlaugust2000@gmail.com>
2026-08-31 11:03:05 -0300

    Remove event listener for audio playback on title click

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const titulo = document.querySelector('header h1');
-    const musica = new Audio('music/vinumsabbathi.mp3');
-    titulo.addEventListener('click', () => {
-        musica.currentTime = 0;
-        musica.play().catch(error => {
-            console.log('Erro ao tentar tocar o áudio:', error);
-        });
-    });
-    titulo.style.cursor = 'pointer';
-});
+
