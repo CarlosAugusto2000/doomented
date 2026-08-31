59da09e606edd2f9b3f72d1b1fabef187df7cccd
CarlosAugusto2000 <carlaugust2000@gmail.com>
2026-08-31 11:05:52 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
-
+const audioTitle = new Audio('music/schizo.mp3');
+const audioText = new Audio('music/quake.mp3');
+const title = document.getElementById('title');
+const welcomeText = document.getElementById('welcome-text');
+title.addEventListener('click', () => { 
+    if (!audioText.paused) {
+        audioText.pause();
+        audioText.currentTime = 0;
+    }
+    if (audioTitle.paused) {
+        audioTitle.play().catch(error => console.log("Erro ao tocar áudio:", error));
+    } else {
+        audioTitle.pause();
+    }
+});
+welcomeText.addEventListener('click', () => {
+    
+    if (!audioTitle.paused) {
+        audioTitle.pause();
+        audioTitle.currentTime = 0;
+    }
+    if (audioText.paused) {
+        audioText.play().catch(error => console.log("Erro ao tocar áudio:", error));
+    } else {
+        audioText.pause();
+    }
+});
+const commentForm = document.getElementById('comment-form');
+commentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Comentário enviado com sucesso!');
+    commentForm.reset();
+});
