abc6844bbfd8c0f39fbf91fd23983ee6fb5c9d2b
CarlosAugusto2000 <carlaugust2000@gmail.com>
2026-08-31 11:08:50 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,33 +1,11 @@
-const audioTitle = new Audio('music/schizo.mp3');
-const audioText = new Audio('music/quake.mp3');
-const title = document.getElementById('title');
-const welcomeText = document.getElementById('welcome-text');
-title.addEventListener('click', () => { 
-    if (!audioText.paused) {
-        audioText.pause();
-        audioText.currentTime = 0;
-    }
-    if (audioTitle.paused) {
-        audioTitle.play().catch(error => console.log("Erro ao tocar áudio:", error));
-    } else {
-        audioTitle.pause();
-    }
-});
-welcomeText.addEventListener('click', () => {
-    
-    if (!audioTitle.paused) {
-        audioTitle.pause();
-        audioTitle.currentTime = 0;
-    }
-    if (audioText.paused) {
-        audioText.play().catch(error => console.log("Erro ao tocar áudio:", error));
-    } else {
-        audioText.pause();
-    }
-});
-const commentForm = document.getElementById('comment-form');
-commentForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Comentário enviado com sucesso!');
-    commentForm.reset();
+document.addEventListener('DOMContentLoaded', () => {
+    const titulo = document.querySelector('header h1');
+    const musica = new Audio('music/vinumsabbathi.mp3');
+    titulo.addEventListener('click', () => {
+        musica.currentTime = 0;
+        musica.play().catch(error => {
+            console.log('Erro ao tentar tocar o áudio:', error);
+        });
+    });
+    titulo.style.cursor = 'pointer';
 });
