634dd9977a47437f4af0377a8e492e8d01e144bf
CarlosAugusto2000 <carlaugust2000@gmail.com>
2026-09-03 11:56:27 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const titulo = document.querySelector('header h1');
-    const musica = new Audio('music/vinumsabbathi.mp3');
+    const caixaHmm = document.querySelector('.full-box');
+    const musicaTitulo = new Audio('music/vinumsabbathi.mp3');
+    const musicaCaixa = new Audio('music/schizo.mp3');
+    const toggleAudio = (audioAtual, outroAudio) => {
+        if (!outroAudio.paused) {
+            outroAudio.pause();
+            outroAudio.currentTime = 0;
+        }
+        if (audioAtual.paused) {
+            audioAtual.play().catch(error => {
+                console.log('Erro ao tentar tocar o áudio:', error);
+            });
+        } else {
+            audioAtual.pause();
+        }
+    };
     titulo.addEventListener('click', () => {
-        musica.currentTime = 0;
-        musica.play().catch(error => {
-            console.log('Erro ao tentar tocar o áudio:', error);
-        });
+        toggleAudio(musicaTitulo, musicaCaixa);
     });
-    titulo.style.cursor = 'pointer';
+    caixaHmm.addEventListener('click', () => {
+        toggleAudio(musicaCaixa, musicaTitulo);
+    });
 });
